8b96d5ddbf1ccbf5947ecfd1706641e944215599
Stelios Malathouras <stelios.malathouras@gmail.com>
2021-06-11 18:01:11 +0300

    add senmdail

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "https://www.ninjutsucon.com/contactform/contactform.php",
+      url: "contactform/contactform.php",
       data: str,
       success: function(msg) {
         // alert(msg);
