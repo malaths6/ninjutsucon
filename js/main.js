58b23c866f5e36fa6da3b0e2c28f935216de145e
Stelios Malathouras <stelios.malathouras@gmail.com>
2021-06-11 17:01:35 +0300

    add senmdail

--- js/main.js
+++ js/main.js
@@ -147,10 +147,5 @@ jQuery(document).ready(function( $ ) {
     var modal = $(this);
     modal.find('#ticket-type').val(ticketType);
   })
-
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
   
 });
